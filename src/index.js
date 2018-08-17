633bdaea991891dfa571aeba1d443247f92e984f
Bastian Schade <rumtraubenuss@googlemail.com>
2018-08-17 15:58:08 +0200

    fix: file name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 
 import './index.css';
-import App from './App';
+import App from './app';
 import client from './apollo-client';
 
 ReactDOM.render((
